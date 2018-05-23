752d045a351e81d89a44d86f9000cd7b2781f3bf
wjmGG <wangjiemengya@foxmail.com>
2018-05-23 23:27:06 +0800

    laravel

---
+++ laravel54/storage/framework/views/4a47d409b2b6ce4d74882dfdd5c44c0acb64e5a5.php
@@ -0,0 +1,38 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">文件上传</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="" enctype="multipart/form-data">
+                        <?php echo e(csrf_field()); ?>
+
+
+
+                        <div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
+                            <label for="password" class="col-md-4 control-label">请选择文件</label>
+
+                            <div class="col-md-6">
+                                <input id="files" type="file" class="form-control" name="source">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    上传
+                                </button>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
